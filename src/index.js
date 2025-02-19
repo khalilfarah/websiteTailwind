0b020a794b0c128d63aa1ecb77b47bb20e4e42c0
khalilfarah <89923289+khalilfarah@users.noreply.github.com>
2025-02-19 22:36:04 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const mobileBtn = document.getElementById('mobileBtn');
+const menuBtn = document.getElementById('menuBtn');
+menuBtn.addEventListener('click', () => {
+
+    mobileBtn.classList.toggle('hidden');
+
+
+
+});
